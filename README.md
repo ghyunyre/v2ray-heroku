41c2a5813f5b2ef6e299304211d8059448627a9a
ghyunyre <97326030+ghyunyre@users.noreply.github.com>
2022-01-08 04:08:02 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# v2ray-heroku
+## 用途: 
+在heroku上搭建v2ray
+## 使用方法:
+1. 注册好heroku账号后打开: https://dashboard.heroku.com/new?template=https://github.com/ghyunyre/v2ray-heroku.git
+2. 填写对应信息
---
+++ configure.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+mkdir /tmp
+cd /tmp
+git clone https://github.com/ghyunyre/v2ray-heroku
+install -m 755 /tmp/v2ray-heroku/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray-heroku/v2ctl /usr/local/bin/v2ctl
+
+rm -rf /tmp/v2ray-heroku
+
+install -d /usr/local/etc/v2ray
+install -d /usr/local/etc/v2ctl
+
+cat << EOF > /usr/local/etc/v2ray/config.json
+{
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "$WSPATH"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+rm /usr/local/etc/v2ray/config.json
